5c2f87129a1610e48e815f085fd31aede8cb28f1
Ruslan Kharkevich <c8man24@gmail.com>
2022-01-17 11:28:09 +0300

    init: start the project

---
+++ Salary/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Salary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.yes_btn = new System.Windows.Forms.Button();
+            this.btn_no = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel = new System.Windows.Forms.Panel();
+            this.panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // yes_btn
+            // 
+            this.yes_btn.Location = new System.Drawing.Point(101, 276);
+            this.yes_btn.Name = "yes_btn";
+            this.yes_btn.Size = new System.Drawing.Size(110, 39);
+            this.yes_btn.TabIndex = 0;
+            this.yes_btn.TabStop = false;
+            this.yes_btn.Text = "да";
+            this.yes_btn.UseVisualStyleBackColor = true;
+            this.yes_btn.Click += new System.EventHandler(this.yes_btn_Click);
+            // 
+            // btn_no
+            // 
+            this.btn_no.Location = new System.Drawing.Point(388, 276);
+            this.btn_no.Name = "btn_no";
+            this.btn_no.Size = new System.Drawing.Size(110, 39);
+            this.btn_no.TabIndex = 1;
+            this.btn_no.TabStop = false;
+            this.btn_no.Text = "нет";
+            this.btn_no.UseVisualStyleBackColor = true;
+            this.btn_no.Click += new System.EventHandler(this.btn_no_Click);
+            this.btn_no.MouseEnter += new System.EventHandler(this.btn_no_Enter);
+            // 
+            // label1
+            // 
+            this.label1.AccessibleRole = System.Windows.Forms.AccessibleRole.Text;
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label1.Font = new System.Drawing.Font("Verdana", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(63, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(457, 34);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Вам нравится ваша зарплата?";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // panel
+            // 
+            this.panel.Controls.Add(this.label1);
+            this.panel.Controls.Add(this.btn_no);
+            this.panel.Controls.Add(this.yes_btn);
+            this.panel.Location = new System.Drawing.Point(0, 0);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(591, 355);
+            this.panel.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 355);
+            this.Controls.Add(this.panel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Belarusbank";
+            this.panel.ResumeLayout(false);
+            this.panel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button yes_btn;
+        private System.Windows.Forms.Button btn_no;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel;
+    }
+}
+
---
+++ Salary/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Salary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            btn_no.BringToFront();
+        }
+
+        private void yes_btn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Мы знали это", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+        }
+
+        private void btn_no_Enter(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int new_x = rnd.Next(btn_no.Size.Width, panel.Size.Width - btn_no.Size.Width);
+            int new_y = rnd.Next(btn_no.Size.Height, panel.Size.Height - btn_no.Size.Height);
+            btn_no.Location = new Point(new_x, new_y);
+            rnd = null;
+        }
+
+        private void btn_no_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Поздравляем, мы повышаем вам зарпоату в 0.9 раз", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+        }
+    }
+}
